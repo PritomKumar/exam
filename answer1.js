70bf883cdbc520a8b965f62bfe89a9d4b37c2e99
PritomKumar <bsse0919@iit.du.ac.bd>
2020-06-22 15:10:29 +0600

    2nd

--- answer1.js
+++ answer1.js
@@ -34,3 +34,23 @@ for (let i=0 ; i< array.length ; i++){
 
 console.log( "Second lowest = " + secondLowest );
 
+let array1 = [4,5,1,2,6,8,9,7,3,24,24,17,19,19,24];
+
+let countnumber=0;
+
+for(let i=0 ;i<array1.length ; i++){
+    let num = array1[i];
+    for(let j=0 ;j<array1.length ; j++){
+        if(array1[i]==array1[j] && array1[i]!=-1){
+            
+            countnumber++;
+            array1[j] = -1;
+            
+        }
+    }
+    console.log(  " Number = " + num + " count = " + countnumber);
+    countnumber=0;
+}
+
+
+
